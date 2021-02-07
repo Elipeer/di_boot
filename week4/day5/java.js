6a7c328f0c3da74cfaf1fdb80ee7775df6978417
Elipeer <elipeer98@gmail.com>
2021-02-07 14:32:11 +0200

    Add files via upload

---
+++ week4/day5/java.js
@@ -0,0 +1,45 @@
+//get smallest distance between numbers
+// let arr = [2,5,3,7,2,3,6,8,6]
+
+// distance = [];
+
+// for (i = 0; i < arr.length - 1; i++) {
+//     if (arr.indexOf(arr[i]) != arr.lastIndexOf(arr[i])) {
+//     	let some = arr.lastIndexOf(arr[i]) - arr.indexOf(arr[i])
+//     	distance.push(some)
+//     }
+// }
+
+// console.log(distance.sort()[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
