86418eb858029eb0425c8a6e81ebf89231c7d694
Elipeer <elipeer98@gmail.com>
2021-02-20 18:45:51 +0200

    Delete day4 directory

--- day4/java.js
+++
@@ -1,6 +0,0 @@
-function compare(arr1,arr2){
-	let difference = arr1.filter(x => !arr2.includes(x)).concat(arr2.filter(x => !arr1.includes(x)));;
-	return difference
-	}
-
-console.log(compare([1,2,3,4,5],[3,2,6,7]))
